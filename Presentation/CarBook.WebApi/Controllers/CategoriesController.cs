c0ce1c25befb95176d11cea591ddde8032451fff
Erdem <erdemsincer123@icloud.com>
2024-10-21 13:25:24 +0300

    Category Sınıfının CQRS işlemleri  yapıldı

---
+++ Presentation/CarBook.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,65 @@
+﻿using CarBook.Application.Features.CQRS.Commands.CategoryCommands;
+using CarBook.Application.Features.CQRS.Handlers.CategoryHandlers;
+using CarBook.Application.Features.CQRS.Queries.CategoryQueries;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly CreateCategoryCommandHandler _createCategoryCommandHandler;
+        private readonly UpdateCategoryCommandHandler _updateCategoryCommandHandler;
+        private readonly RemoveCategoryCommandHandler _removeCategoryCommandHandler;
+        private readonly GetCategoryByIdQueryHandler _getCategoryByIdQueryHandler;
+        private readonly GetCategoryQueryHandler _getCategoryQueryHandler;
+
+
+        public CategoriesController(CreateCategoryCommandHandler createCategoryCommandHandler,
+            UpdateCategoryCommandHandler updateCategoryCommandHandler,
+            RemoveCategoryCommandHandler removeCategoryCommandHandler,
+            GetCategoryByIdQueryHandler getCategoryByIdQueryHandler,
+            GetCategoryQueryHandler getCategoryQueryHandler)
+        {
+            _createCategoryCommandHandler = createCategoryCommandHandler;
+            _updateCategoryCommandHandler = updateCategoryCommandHandler;
+            _removeCategoryCommandHandler = removeCategoryCommandHandler;
+            _getCategoryByIdQueryHandler = getCategoryByIdQueryHandler;
+            _getCategoryQueryHandler = getCategoryQueryHandler;
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> CategoryList()
+        {
+            var values = await _getCategoryQueryHandler.Handle();
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCategory(int id)
+        {
+            var values = await _getCategoryByIdQueryHandler.Handle(new GetCategoryByIdQuery(id));
+            return Ok(values);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CrateCategory(CreateCategoryCommands createCategoryCommand)
+        {
+            await _createCategoryCommandHandler.Handle(createCategoryCommand);
+            return Ok("Category bilgisi eklendi");
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteCategory(int id)
+        {
+            await _removeCategoryCommandHandler.Handle(new RemoveCategoryCommands(id));
+            return Ok("Category bilgisi silindi");
+
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateCategory(UpdateCategoryCommands updateCategoryCommand)
+        {
+            await _updateCategoryCommandHandler.Handle(updateCategoryCommand);
+            return Ok("Category Bilgisi güncellendi");
+        }
+    }
+}
