27236da9c228279349b13c7bd725372b306597e8
Erdem <erdemsincer123@icloud.com>
2024-10-22 13:35:37 +0300

    Feature Api Yazıldı

--- Core/CarBook.Application/Features/Mediator/Handlers/FeatureHandlers/CreateFeatureCommandHandler.cs
+++ Core/CarBook.Application/Features/Mediator/Handlers/FeatureHandlers/CreateFeatureCommandHandler.cs
@@ -2,24 +2,22 @@
 using CarBook.Application.Interfaces;
 using CarBook.Domain.Entities;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CarBook.Application.Features.Mediator.Handlers.FeatureHandlers
+public class CreateFeatureCommandHandler : IRequestHandler<CreateFeatureCommand>
 {
-    public class CreateFeatureCommandHandler : IRequestHandler<CreateFeatureCommand>
+    private readonly IRepository<Feature> _repository;
+
+    // Dependency Injection ile IRepository'yi alıyoruz
+    public CreateFeatureCommandHandler(IRepository<Feature> repository)
     {
-        private readonly IRepository<Feature> _repository;
-        public async Task Handle(CreateFeatureCommand request, CancellationToken cancellationToken)
+        _repository = repository;
+    }
+
+    public async Task Handle(CreateFeatureCommand request, CancellationToken cancellationToken)
+    {
+        await _repository.CreateAsync(new Feature
         {
-            await _repository.CreateAsync(new Feature
-            {
-                Name = request.Name
-            });
-            
-        }
+            Name = request.Name
+        });
     }
 }
--- Presentation/CarBook.WebApi/Program.cs
+++ Presentation/CarBook.WebApi/Program.cs
@@ -6,6 +6,7 @@ using CarBook.Application.Features.CQRS.Handlers.CategoryHandlers;
 using CarBook.Application.Features.CQRS.Handlers.ContactHandlers;
 using CarBook.Application.Interfaces;
 using CarBook.Application.Interfaces.CarInterfaces;
+using CarBook.Application.Services;
 using CarBook.Domain.Entities;
 using CarBook.Persistence.Context;
 using CarBook.Persistence.Repositories;
@@ -55,6 +56,8 @@ builder.Services.AddScoped<GetContactByIdQueryHandler>();
 builder.Services.AddScoped<CreateContactCommandHandler>();
 builder.Services.AddScoped<UpdateContactCommandHandler>();
 builder.Services.AddScoped<RemoveContactCommandHandler>();
+
+builder.Services.AddApplicationService(builder.Configuration);
 
 
 builder.Services.AddControllers();
