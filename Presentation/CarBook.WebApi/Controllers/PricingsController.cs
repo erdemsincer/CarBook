3a04d66b4a8dc07bb6791745291ee84c803011c9
Erdem <erdemsincer123@icloud.com>
2024-10-24 00:53:57 +0300

    Pricing CQRS işlemleri yapıldı

---
+++ Presentation/CarBook.WebApi/Controllers/PricingsController.cs
@@ -0,0 +1,53 @@
+﻿using CarBook.Application.Features.Mediator.Commands.PricingCommand;
+using CarBook.Application.Features.Mediator.Queries.PricingQueries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PricingsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public PricingsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet]
+
+        public async Task<IActionResult> PricingList()
+        {
+            var values = await _mediator.Send(new GetPricingQuery());
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+
+        public async Task<IActionResult> GetPricing(int id)
+        {
+            var values = await _mediator.Send(new GetPricingByIdQuery(id));
+            return Ok(values);
+        }
+        [HttpPost]
+
+        public async Task<IActionResult> CreatePricing(CreatePricingCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Pricing Başarı ile eklendi");
+        }
+        [HttpDelete]
+        public async Task<IActionResult> RemovePricing(int id)
+        {
+            await _mediator.Send(new RemovePricingCommand(id));
+            return Ok("Pricing Başarı ile silindi");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdatePricing(UpdatePricingCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Pricing Başarı ile güncellendi");
+        }
+    }
+}
