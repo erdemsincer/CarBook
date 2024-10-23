8a2fc65f341961efa3559b74a390b68cc12a8f6f
Erdem <erdemsincer123@icloud.com>
2024-10-23 11:48:33 +0300

    Location CQRS işlemleri yapıldı

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/LocationHandlers/GetLocationByIdQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using CarBook.Application.Features.Mediator.Queries.LocationQueries;
+using CarBook.Application.Features.Mediator.Results.LocationResults;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.Mediator.Handlers.LocationHandlers
+{
+    public class GetLocationByIdQueryHandler : IRequestHandler<GetLocationByIdQuery, GetLocationByIdQueryResults>
+    {
+        private readonly IRepository<Location> _repository;
+
+        public GetLocationByIdQueryHandler(IRepository<Location> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetLocationByIdQueryResults> Handle(GetLocationByIdQuery request, CancellationToken cancellationToken)
+        {
+            var values=await _repository.GetByIdAsync(request.Id);
+            return new GetLocationByIdQueryResults
+            {
+                LocationId = values.LocationId,
+                Name = values.Name,
+
+            };
+        }
+    }
+}
