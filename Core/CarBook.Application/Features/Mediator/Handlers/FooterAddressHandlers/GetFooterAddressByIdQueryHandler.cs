70eedbce206c12c11ebd78d17fe8262cedf361a2
Erdem <erdemsincer123@icloud.com>
2024-10-23 11:05:56 +0300

    FooterAddress CQRS işlemleri yapıldı

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/FooterAddressHandlers/GetFooterAddressByIdQueryHandler.cs
@@ -0,0 +1,40 @@
+﻿
+using CarBook.Application.Features.Mediator.Queries.FooterAddressQueries;
+
+using CarBook.Application.Features.Mediator.Results.FooterAddressResults;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.Mediator.Handlers.FooterAddressHandlers
+{
+    public class GetFooterAddressByIdQueryHandler : IRequestHandler<GetFooterAddressByIdQuery, GetFooterAddressByIdQueryResults>
+    {
+
+        private readonly IRepository<FooterAddress> _repository;
+
+        public GetFooterAddressByIdQueryHandler(IRepository<FooterAddress> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetFooterAddressByIdQueryResults> Handle(GetFooterAddressByIdQuery request, CancellationToken cancellationToken)
+        {
+            var values=await _repository.GetByIdAsync(request.Id);
+            return new GetFooterAddressByIdQueryResults
+            {
+                Address = values.Address,
+                FooterAddressId = values.FooterAddressId,
+                Description = values.Description,
+                Email = values.Email,
+                Phone = values.Phone,
+
+            };
+        }
+    }
+}
