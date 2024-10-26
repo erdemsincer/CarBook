16b2458f888776248eaddfbad165d71d19219424
Erdem <erdemsincer123@icloud.com>
2024-10-26 13:40:41 +0300

    Template parçalandı ve PartialViewler oluşturuldu

---
+++ Frontends/CarBook.webUI/ViewComponents/UILayoutComponents/_FooterUILayoutComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.webUI.ViewComponents.UILayoutComponents
+{
+    public class _FooterUILayoutComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke() { return View(); }
+    }
+}
---
+++ Frontends/CarBook.webUI/ViewComponents/UILayoutComponents/_MainCoverUILayoutComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.webUI.ViewComponents.UILayoutComponents
+{
+    public class _MainCoverUILayoutComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke() { return View(); }
+    }
+}
