b58133394f5d5e77a4dd5f7dee9019e474e5cb0b
Erdem <erdemsincer123@icloud.com>
2024-10-28 16:17:24 +0300

    About view ekranı dinamik hale geteirildi

--- Frontends/CarBook.webUI/ViewComponents/AboutViewComponents/_AboutUsComponentPartial.cs
+++ Frontends/CarBook.webUI/ViewComponents/AboutViewComponents/_AboutUsComponentPartial.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.AboutDtos;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
 namespace CarBook.webUI.ViewComponents.AboutViewComponents
@@ -19,7 +20,8 @@ namespace CarBook.webUI.ViewComponents.AboutViewComponents
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData= await responseMessage.Content.ReadAsStringAsync();
-                var values=JsonConvert.DeserializeObject<List<>>(jsonData);
+                var values=JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
+                return View(values);
             }
             return View();
         }
