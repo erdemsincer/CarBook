87c641dd63de2dc483da0b2c94331f944667ab38
Erdem <erdemsincer123@icloud.com>
2024-11-11 00:39:22 +0300

    Mesaj Gönderme işlemi yapıldı

--- Frontends/CarBook.webUI/Controllers/ContactController.cs
+++ Frontends/CarBook.webUI/Controllers/ContactController.cs
@@ -2,6 +2,7 @@
 using CarBook.Dto.LocationDtos;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace CarBook.webUI.Controllers
 {
@@ -13,18 +14,25 @@ namespace CarBook.webUI.Controllers
         {
             _httpClientFactory = httpClientFactory;
         }
-
-        public async Task<IActionResult> Index()
+        [HttpGet]
+        public IActionResult Index()
         {
-            var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("http://localhost:5026/api/Contacts");
-            if (responseMessage.IsSuccessStatusCode)
-            {
-                var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<ResultContactDto>>(jsonData);
-                return View(values);
-            }
             return View();
         }
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateContactDtos createContactDtos)
+        {
+            var client = _httpClientFactory.CreateClient();
+            createContactDtos.SendDate = DateTime.Now;
+            var jsonData=JsonConvert.SerializeObject(createContactDtos);
+            StringContent content = new StringContent(jsonData,Encoding.UTF8,"application/json");
+            var responseMessage = await client.PostAsync("http://localhost:5026/api/Contacts",content);
+            if (responseMessage.IsSuccessStatusCode) { 
+              return RedirectToAction("Index","Default");
+
+            }
+            return View();
+
+		}
     }
 }
