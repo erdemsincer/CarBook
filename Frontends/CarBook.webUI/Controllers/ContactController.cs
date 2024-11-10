03b707930016701cce0168132046b1c5f039f26c
Erdem <erdemsincer123@icloud.com>
2024-11-10 17:02:17 +0300

    Car

---
+++ Frontends/CarBook.webUI/Controllers/ContactController.cs
@@ -0,0 +1,30 @@
+﻿using CarBook.Dto.ContactDtos;
+using CarBook.Dto.LocationDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.webUI.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ContactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:5026/api/Contacts");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultContactDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
---
+++ Frontends/CarBook.webUI/Controllers/LocationController.cs
@@ -0,0 +1,30 @@
+﻿using CarBook.Dto.CarDtos;
+using CarBook.Dto.LocationDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.webUI.Controllers
+{
+    public class LocationController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public LocationController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:5026/api/Locations");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultLocationDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
