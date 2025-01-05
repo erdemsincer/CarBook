addec67c313eaf4f16d4dae4a9ef339744b7be1a
Erdem <erdemsincer123@icloud.com>
2025-01-05 03:59:53 +0300

    Bloglar frontta Listelendi

---
+++ Frontends/CarBook.webUI/Controllers/BlogController.cs
@@ -0,0 +1,31 @@
+﻿using CarBook.Dto.BlogDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.webUI.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BlogController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task< IActionResult> Index()
+        {
+            ViewBag.v1 = "Bloglar";
+            ViewBag.v2 = "Yazarlarımızın Blogları";
+            var client =_httpClientFactory.CreateClient();
+            var ResponseMessage = await client.GetAsync("http://localhost:5026/api/Blogs/GetAllBlogsWithAuthorList");
+            if (ResponseMessage.IsSuccessStatusCode)
+            {
+                var jsonData=await ResponseMessage.Content.ReadAsStringAsync();
+                var values=JsonConvert.DeserializeObject<List<ResultAllBlogsWithAuthorDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
