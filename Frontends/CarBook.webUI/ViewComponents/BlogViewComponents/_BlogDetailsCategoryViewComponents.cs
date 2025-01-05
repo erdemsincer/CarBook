5546f0e748e09c094d9e14059399681c8dd9fdad
Erdem <erdemsincer123@icloud.com>
2025-01-05 21:02:04 +0300

    Blog Categori dinamikleştirildi

--- Frontends/CarBook.webUI/ViewComponents/BlogViewComponents/_BlogDetailsCategoryViewComponents.cs
+++ Frontends/CarBook.webUI/ViewComponents/BlogViewComponents/_BlogDetailsCategoryViewComponents.cs
@@ -1,11 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.AboutDtos;
+using CarBook.Dto.CategoryDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBook.webUI.ViewComponents.BlogViewComponents
 {
     public class _BlogDetailsCategoryViewComponents:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _BlogDetailsCategoryViewComponents(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client=_httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:5026/api/Categories");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategorydto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
