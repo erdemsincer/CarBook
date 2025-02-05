29c49e1885a8dd1a4c358b7201b01d6303d05714
Erdem <erdemsincer123@icloud.com>
2025-02-05 23:56:56 +0300

    Araba Girişi ve araba giriş ekranı tasarland ve dropdawn olarak brand getirildi

--- Frontends/CarBook.webUI/Controllers/AdminCarController.cs
+++ Frontends/CarBook.webUI/Controllers/AdminCarController.cs
@@ -1,6 +1,9 @@
-﻿using CarBook.Dto.CarDtos;
+﻿using CarBook.Dto.BrandDtos;
+using CarBook.Dto.CarDtos;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace CarBook.webUI.Controllers
 {
@@ -22,6 +25,36 @@ namespace CarBook.webUI.Controllers
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
                 var values= JsonConvert.DeserializeObject<List<ResultCarWithBrandsDtos>>(jsonData);
                 return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<  IActionResult> CreateCar()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7015/api/Brands");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultBrandDto>>(jsonData);
+            List<SelectListItem> brandValues = (from x in values
+                                                select new SelectListItem
+                                                {
+                                                    Text = x.name,
+                                                    Value = x.brandID.ToString()
+                                                }).ToList();
+            ViewBag.BrandValues = brandValues;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCar(CreateCarDto createCarDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createCarDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7015/api/Cars", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
             }
             return View();
         }
