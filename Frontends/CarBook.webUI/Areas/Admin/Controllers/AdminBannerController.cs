88d50b4af4048de7c085255b499066160679bc84
Erdem <erdemsincer123@icloud.com>
2025-02-07 01:12:52 +0300

    Banner Listeleme işlemi ve admin areasi

---
+++ Frontends/CarBook.webUI/Areas/Admin/Controllers/AdminBannerController.cs
@@ -0,0 +1,35 @@
+﻿using CarBook.Dto.BannerDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace CarBook.webUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/AdminBanner")]
+    public class AdminBannerController : Controller
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public AdminBannerController(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            var client = _clientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7015/api/Banners");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBannerDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
+
+
