afb7489986af89f3f6e5c48567e65c9434dd73c0
Erdem <erdemsincer123@icloud.com>
2025-02-08 01:03:21 +0300

    Gelen mesajları Listeleme

---
+++ Frontends/CarBook.webUI/Areas/Admin/Controllers/AdminContactController.cs
@@ -0,0 +1,34 @@
+﻿using CarBook.Dto.ContactDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.webUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/AdminContact")]
+    public class AdminContactController : Controller
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public AdminContactController(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+
+        [Route("Index")]
+
+        public async Task< IActionResult> Index()
+        {
+            var client = _clientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7015/api/Contacts");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultContactDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
