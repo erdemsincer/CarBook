b1e967bee75692e364fcd58723345d8d43187096
Erdem <erdemsincer123@icloud.com>
2025-02-07 22:56:44 +0300

    Blog Listeleme işlemi yapıldı

---
+++ Frontends/CarBook.webUI/Areas/Admin/Controllers/AdminBlogController.cs
@@ -0,0 +1,45 @@
+﻿using CarBook.Dto.BlogDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.webUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/AdminBlog")]
+    public class AdminBlogController : Controller
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public AdminBlogController(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            var client = _clientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7015/api/Blogs/GetAllBlogsWithAuthorList");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAllBlogsWithAuthorDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [Route("RemoveBlog/{id}")]
+        public async Task<IActionResult> RemoveBlog(int id)
+        {
+            var client = _clientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:7015/api/Blogs/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminBlog", new { area = "Admin" });
+            }
+            return View();
+        }
+    }
+}
