fad4184d33d67194a5833a2bf4c83012608e53d2
Erdem <erdemsincer123@icloud.com>
2025-02-08 00:50:19 +0300

    fiyattlandırma ekleme silme güncelleme yapıldı

--- Presentation/CarBook.WebApi/Controllers/PricingsController.cs
+++ Presentation/CarBook.WebApi/Controllers/PricingsController.cs
@@ -37,7 +37,7 @@ namespace CarBook.WebApi.Controllers
             await _mediator.Send(command);
             return Ok("Pricing Başarı ile eklendi");
         }
-        [HttpDelete]
+        [HttpDelete("{id}")]
         public async Task<IActionResult> RemovePricing(int id)
         {
             await _mediator.Send(new RemovePricingCommand(id));
