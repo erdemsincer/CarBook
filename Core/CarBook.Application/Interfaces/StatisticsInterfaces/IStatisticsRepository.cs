f0ab780671cdada1184aa59c6243cb832ba37cb1
Erdem <erdemsincer123@icloud.com>
2025-02-09 15:35:59 +0300

    Istatisticrepository hazırlandı

---
+++ Core/CarBook.Application/Interfaces/StatisticsInterfaces/IStatisticsRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Interfaces.StatisticsInterfaces
+{
+    public interface IStatisticsRepository
+    {
+        int LocationCount();
+        int GetAuthorCount();
+        int GetBrandCount();
+        int GetBlogCount();
+        double GetAvgRentPriceForDaily();
+        double GetAvgRentPriceForWeekly();
+        double GetAvgRentPriceForMonthly();
+        int GetCarCountByTranmissionIsAuto();
+        string BrandNameByMaxCar();
+        string BlogTitleByMaxBlogCount();
+        int GetCarCountByKmSmallerThen1000();
+        int GetCarCountByFuelGasolineOrDiesel();
+        int GetCarCountByFuelElectric();
+        string GetCarBrandAndModelByRentPriceDailyMax();
+        string GetCarBrandAndModelByRentPriceDailyMin();
+    }
+}
---
+++ Infrastructure/CarBook.Persistence/Repositories/StatisticsRepositories/StatisticsRepositories.cs
@@ -0,0 +1,87 @@
+﻿using CarBook.Application.Interfaces.StatisticsInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Persistence.Repositories.StatisticsRepositories
+{
+    public class StatisticsRepositories : IStatisticsRepository
+    {
+        public string BlogTitleByMaxBlogCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string BrandNameByMaxCar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetAuthorCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetAvgRentPriceForDaily()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetAvgRentPriceForMonthly()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetAvgRentPriceForWeekly()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetBlogCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetBrandCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetCarBrandAndModelByRentPriceDailyMax()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetCarBrandAndModelByRentPriceDailyMin()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetCarCountByFuelElectric()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetCarCountByFuelGasolineOrDiesel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetCarCountByKmSmallerThen1000()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetCarCountByTranmissionIsAuto()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int LocationCount()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
