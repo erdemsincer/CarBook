3fc6b6a6fddbace2ce0fb0edcf9a382c65ae5377
Erdem <erdemsincer123@icloud.com>
2025-02-10 19:13:43 +0300

    İstatistik görüntüleme yapılmaya başladnı

--- Frontends/CarBook.webUI/Areas/Admin/Controllers/AdminStatisticsController.cs
+++ Frontends/CarBook.webUI/Areas/Admin/Controllers/AdminStatisticsController.cs
@@ -30,7 +30,47 @@ namespace CarBook.webUI.Areas.Admin.Controllers
                 ViewBag.v = values.carCount;
                 ViewBag.v1=v1;
             }
+
+            var client2 = _clientFactory.CreateClient();
+            var responseMessage2 = await client.GetAsync("https://localhost:7015/api/Statistics/GetBrandCount");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                int brandCountRandom = random.Next(0, 101);
+                var jsonData2 = await responseMessage2.Content.ReadAsStringAsync();
+                var values2 = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonData2);
+                ViewBag.brandCount = values2.brandCount;
+                ViewBag.brandCountRandom = brandCountRandom;
+            }
+            var responseMessage4 = await client.GetAsync("https://localhost:7015/api/Statistics/GetBlogCount");
+            if (responseMessage4.IsSuccessStatusCode)
+            {
+                int blogCountRandom = random.Next(0, 101);
+                var jsonData4 = await responseMessage4.Content.ReadAsStringAsync();
+                var values4 = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonData4);
+                ViewBag.blogCount = values4.blogCount;
+                ViewBag.blogCountRandom = blogCountRandom;
+            }
+            var responseMessage5 = await client.GetAsync("https://localhost:7015/api/Statistics/GetBrandCount");
+            if (responseMessage5.IsSuccessStatusCode)
+            {
+                int brandCountRandom = random.Next(0, 101);
+                var jsonData5 = await responseMessage5.Content.ReadAsStringAsync();
+                var values5 = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonData5);
+                ViewBag.brandCount = values5.brandCount;
+                ViewBag.brandCountRandom = brandCountRandom;
+            }
+
+            var responseMessage6 = await client.GetAsync("https://localhost:7015/api/Statistics/GetAvgRentPriceForDaily");
+            if (responseMessage6.IsSuccessStatusCode)
+            {
+                int avgRentPriceForDailyRandom = random.Next(0, 101);
+                var jsonData6 = await responseMessage6.Content.ReadAsStringAsync();
+                var values6 = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonData6);
+                ViewBag.avgRentPriceForDaily = values6.avgPriceForDaily;
+                ViewBag.avgRentPriceForDailyRandom = avgRentPriceForDailyRandom;
+            }
             return View();
+         
         }
     }
 }
