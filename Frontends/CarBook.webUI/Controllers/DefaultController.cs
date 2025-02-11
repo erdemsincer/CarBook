9c159f9b41095ba15f0d5609ceba04ad390d45b2
Erdem <erdemsincer123@icloud.com>
2025-02-12 00:29:07 +0300

    Lokasyonları dropdownda listeleme

--- Frontends/CarBook.webUI/Controllers/DefaultController.cs
+++ Frontends/CarBook.webUI/Controllers/DefaultController.cs
@@ -1,12 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.LocationDtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
 
 namespace CarBook.webUI.Controllers
 {
     public class DefaultController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public DefaultController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        [HttpGet]
+        public async Task< IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7015/api/Locations");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultLocationDto>>(jsonData);
+            List<SelectListItem> values2 = (from x in values
+                                            select new SelectListItem
+                                            {
+                                                Text = x.Name,
+                                                Value =x.LocationId.ToString()
+                                            }).ToList();
+            ViewBag.v = values2;
             return View();
+           
         }
     }
 }
