960a09028cfa9c00d4dd152ed2a0ad596eed380d
Erdem <erdemsincer123@icloud.com>
2025-02-12 23:22:59 +0300

    Reservation Entity

---
+++ Core/CarBook.Domain/Entities/Reservation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Domain.Entities
+{
+    public class Reservation
+    {
+        public int ReservationId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public int CarId { get; set; }
+     
+        //public string PickUpLocationId { get; set; }
+        public int Age { get; set; }
+        public int DriverLicanseYear { get; set; }
+        public string? Description { get; set; }
+    }
+}
