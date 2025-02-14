b2c079aa0ec50fa8541dad3f083f8704e81a5b28
Erdem <erdemsincer123@icloud.com>
2025-02-14 16:17:03 +0300

    Araç fiyat listesi yapıldı

--- Frontends/CarBook.webUI/Controllers/CarPricingController.cs
+++ Frontends/CarBook.webUI/Controllers/CarPricingController.cs
@@ -1,13 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.CarPricingDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBook.webUI.Controllers
 {
     public class CarPricingController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _clientFactory;
+
+        public CarPricingController(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        public async Task< IActionResult> Index()
         {
             ViewBag.v1 = "Paketler";
             ViewBag.v2 = "Araç Fiyat Paketleri";
+            var client = _clientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7015/api/CarPricing/GetCarPricingWithTimePeriodList");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCarPricingListWithModelDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
