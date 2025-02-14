d4f28eb4e2995969e4c52fc1563aa6289769cc89
Erdem <erdemsincer123@icloud.com>
2025-02-14 16:53:34 +0300

    FooterAddress kısmı yapıldı

--- Frontends/CarBook.webUI/ViewComponents/UILayoutComponents/_FooterUILayoutComponentPartial.cs
+++ Frontends/CarBook.webUI/ViewComponents/UILayoutComponents/_FooterUILayoutComponentPartial.cs
@@ -1,9 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.FooterAddressDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBook.webUI.ViewComponents.UILayoutComponents
 {
     public class _FooterUILayoutComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke() { return View(); }
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _FooterUILayoutComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task< IViewComponentResult> InvokeAsync() {
+
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7015/api/FooterAddresses");
+            if (response.IsSuccessStatusCode)
+            {
+                var responseString = await response.Content.ReadAsStringAsync();
+                var footerAddresses = JsonConvert.DeserializeObject<List<ResultFooterAddressDto>>(responseString);
+                return View(footerAddresses);
+            }
+            return View(); 
+        }
     }
 }
