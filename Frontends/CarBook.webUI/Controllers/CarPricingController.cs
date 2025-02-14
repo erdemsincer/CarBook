215e54b2476d4f9e24777247edfda7099173d5c9
Erdem <erdemsincer123@icloud.com>
2025-02-14 03:16:00 +0300

    ARABA FİYAT LİSTESİ SAYFASI OLUŞTUURLUDU

---
+++ Frontends/CarBook.webUI/Controllers/CarPricingController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.webUI.Controllers
+{
+    public class CarPricingController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Hizmetler";
+            ViewBag.v2 = "Hizmetlerimiz";
+            return View();
+        }
+    }
+}
