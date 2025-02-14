69a551fdf20cb0261c61b4815e59b2079fa38e2a
Erdem <erdemsincer123@icloud.com>
2025-02-15 00:26:35 +0300

    Blog yorum görüntüleme

--- Frontends/CarBook.webUI/ViewComponents/CommentViewComponents/_CommentListByBlogComponentPartial.cs
+++ Frontends/CarBook.webUI/ViewComponents/CommentViewComponents/_CommentListByBlogComponentPartial.cs
@@ -1,11 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.CommentDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBook.webUI.ViewComponents.CommentViewComponents
 {
     public class _CommentListByBlogComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _CommentListByBlogComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task< IViewComponentResult> InvokeAsync(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            ViewBag.blogid = id;
+            var responseMessage = await client.GetAsync("https://localhost:7015/api/Comments/CommentListByBlog?id="+id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCommentDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
