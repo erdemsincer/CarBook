325782e340e237262adfda86a1ddbc956be363ad
Erdem <erdemsincer123@icloud.com>
2025-02-16 01:35:06 +0300

    mediatör işlemleri yapılmaya başlandı

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/CarFeatureHandlers/UpdateCarFeatureAvailableChangeToTrueCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using CarBook.Application.Features.Mediator.Commands.CarFeatureCommand;
+using CarBook.Application.Interfaces.CarFeatureInterfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.Mediator.Handlers.CarFeatureHandlers
+{
+    public class UpdateCarFeatureAvailableChangeToTrueCommandHandler : IRequestHandler<UpdateCarFeatureAvailableChangeToTrueCommand>
+    {
+        private readonly ICarFeatureRepository _carFeatureRepository;
+
+        public UpdateCarFeatureAvailableChangeToTrueCommandHandler(ICarFeatureRepository carFeatureRepository)
+        {
+            _carFeatureRepository = carFeatureRepository;
+        }
+
+        public async Task Handle(UpdateCarFeatureAvailableChangeToTrueCommand request, CancellationToken cancellationToken)
+        {
+            _carFeatureRepository.ChangeCarFeatureAvailableToTrue(request.Id);
+        }
+    }
+}
