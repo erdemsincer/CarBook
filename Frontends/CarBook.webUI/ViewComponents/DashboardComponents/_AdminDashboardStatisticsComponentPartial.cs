c0af802728d48edded19fb9aa4848b0b19705759
Erdem <erdemsincer123@icloud.com>
2025-02-16 15:55:27 +0300

    Admin dashboard statistic yapıldı

---
+++ Frontends/CarBook.webUI/ViewComponents/DashboardComponents/_AdminDashboardStatisticsComponentPartial.cs
@@ -0,0 +1,64 @@
+﻿using CarBook.Dto.StatisticsDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace CarBook.webUI.ViewComponents.DashboardComponents
+{
+    public class _AdminDashboardStatisticsComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public _AdminDashboardStatisticsComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _clientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            Random random = new Random();
+            var client = _clientFactory.CreateClient();
+
+            
+            var responseMessage = await client.GetAsync("https://localhost:7015/api/Statistics/GetCarCount");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                int v1 = random.Next(0, 101);
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonData);
+                ViewBag.v = values.carCount;
+                ViewBag.v1 = v1;
+            }
+
+            var responseMessage2 = await client.GetAsync("https://localhost:7015/api/Statistics/GetLocationCount");
+            if (responseMessage2.IsSuccessStatusCode)
+            {
+                int locationCountRandom = random.Next(0, 101);
+                var jsonData2 = await responseMessage2.Content.ReadAsStringAsync();
+                var values2 = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonData2);
+                ViewBag.locationCount = values2.locationCount;
+                ViewBag.locationCountRandom = locationCountRandom;
+            }
+            var responseMessage3 = await client.GetAsync("https://localhost:7015/api/Statistics/GetBrandCount");
+            if (responseMessage3.IsSuccessStatusCode)
+            {
+                int brandCountRandom = random.Next(0, 101);
+                var jsonData3 = await responseMessage3.Content.ReadAsStringAsync();
+                var values3 = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonData3);
+                ViewBag.brandCount = values3.brandCount;
+                ViewBag.brandCountRandom = brandCountRandom;
+            }
+            var responseMessage4 = await client.GetAsync("https://localhost:7015/api/Statistics/GetAvgRentPriceForDaily");
+            if (responseMessage4.IsSuccessStatusCode)
+            {
+                int avgRentPriceForDailyRandom = random.Next(0, 101);
+                var jsonData4 = await responseMessage4.Content.ReadAsStringAsync();
+                var values4 = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonData4);
+                ViewBag.avgRentPriceForDaily = values4.avgPriceForDaily.ToString("0.00"); ;
+                ViewBag.avgRentPriceForDailyRandom = avgRentPriceForDailyRandom;
+            }
+
+            return View();
+        }
+    }
+}
