f3f5fa1f62db60d6ca6141db20d3f2ee42d18c36
Erdem <erdemsincer123@icloud.com>
2025-02-16 20:27:11 +0300

    Grafikler yapıldı admin dashboard

---
+++ Frontends/CarBook.webUI/ViewComponents/DashboardComponents/_AdminDashboardChart2ComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.webUI.ViewComponents.DashboardComponents
+{
+    public class _AdminDashboardChart2ComponentPartial: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
