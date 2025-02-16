12054853764d425c872fa2180acbf0ad67895710
Erdem <erdemsincer123@icloud.com>
2025-02-16 16:04:42 +0300

    Admin dashboard car fiyatları

---
+++ Frontends/CarBook.webUI/ViewComponents/DashboardComponents/_AdminDashboardCarPricingListComponentPartial.cs
@@ -0,0 +1,31 @@
+﻿using CarBook.Dto.CarPricingDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.webUI.ViewComponents.DashboardComponents
+{
+    public class _AdminDashboardCarPricingListComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _AdminDashboardCarPricingListComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7015/api/CarPricing/GetCarPricingWithTimePeriodList");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var carPricingList = JsonConvert.DeserializeObject<List<ResultCarPricingListWithModelDto>>(jsonData);
+                return View(carPricingList);
+            }
+        
+
+            return View();
+        }
+    }
+}
