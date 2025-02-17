79205cf9a92645c6215caf7db8489973e4cbc7eb
Erdem <erdemsincer123@icloud.com>
2025-02-18 00:49:16 +0300

    Appuser query ve result sınıfları

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/AppUserHandlers/GetCheckAppUserQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using CarBook.Application.Features.Mediator.Queries.AppUserQueries;
+using CarBook.Application.Features.Mediator.Results.AppUserResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.Mediator.Handlers.AppUserHandlers
+{
+	public class GetCheckAppUserQueryHandler : IRequestHandler<GetCheckAppUserQuery, GetCheckAppUserQueryResult>
+	{
+		public Task<GetCheckAppUserQueryResult> Handle(GetCheckAppUserQuery request, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+	}
+	
+}
