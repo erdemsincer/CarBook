11c8b0c2bdd51927b09d487b41a166e6dd8650d5
Erdem <erdemsincer123@icloud.com>
2025-02-17 15:31:16 +0300

    Review Mediatör işlemleri

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/ReviewHandlers/GetReviewByCarIdQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using CarBook.Application.Features.Mediator.Queries.ReviewQueries;
+using CarBook.Application.Features.Mediator.Results.ReviewResults;
+using CarBook.Application.Interfaces.ReviewInterfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.Mediator.Handlers.ReviewHandlers
+{
+	public class GetReviewByCarIdQueryHandler : IRequestHandler<GetReviewByCarIdQuery, List<GetReviewByCarIdQueryResult>>
+	{
+		private readonly IReviewRepository _reviewRepository;
+
+		public GetReviewByCarIdQueryHandler(IReviewRepository reviewRepository)
+		{
+			_reviewRepository = reviewRepository;
+		}
+
+		public async Task<List<GetReviewByCarIdQueryResult>> Handle(GetReviewByCarIdQuery request, CancellationToken cancellationToken)
+		{
+			var values=_reviewRepository.GetReviewsByCarId(request.Id);
+			return values.Select(x=>new GetReviewByCarIdQueryResult
+			{
+				CarId = x.CarId,
+				Comment = x.Comment,
+				ReviewDate=x.ReviewDate,
+				CustomerImage= x.CustomerImage,	
+				CustomerName= x.CustomerName,
+				RaytingValue=x.RaytingValue,
+				ReviewId = x.ReviewId
+			}).ToList();
+		}
+	}
+	
+}
