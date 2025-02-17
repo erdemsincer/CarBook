e626f80818c14800e415af33336d7c3a8d2d9c45
Erdem <erdemsincer123@icloud.com>
2025-02-17 23:02:00 +0300

    Review create ve update review

--- Presentation/CarBook.WebApi/Controllers/ReviewsController.cs
+++ Presentation/CarBook.WebApi/Controllers/ReviewsController.cs
@@ -1,4 +1,5 @@
-﻿using CarBook.Application.Features.Mediator.Queries.ReviewQueries;
+﻿using CarBook.Application.Features.Mediator.Commands.ReviewCommand;
+using CarBook.Application.Features.Mediator.Queries.ReviewQueries;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -22,5 +23,23 @@ namespace CarBook.WebApi.Controllers
 			var values=await _mediator.Send(new GetReviewByCarIdQuery(id));
 			return Ok(values);
 		}
+
+		[HttpPost]
+
+		public async Task<IActionResult> CreateReview(CreateReviewCommand command)
+		{
+		    await _mediator.Send(command);
+			return Ok("eklendi");
+		}
+
+		[HttpPut]
+
+		public async Task<IActionResult> UpdateReview(UpdateReviewCommand command)
+		{
+			await _mediator.Send(command);
+			return Ok("Güncellendi");
+		}
+
+
 	}
 }
