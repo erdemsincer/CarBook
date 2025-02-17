6e6002f389f12d64e00033291c75dfb59c3a97c1
Erdem <erdemsincer123@icloud.com>
2025-02-17 22:43:06 +0300

    Update review command handler

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/ReviewHandlers/UpdateReviewHandler.cs
@@ -0,0 +1,36 @@
+﻿using CarBook.Application.Features.Mediator.Commands.ReviewCommand;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.Mediator.Handlers.ReviewHandlers
+{
+	public class UpdateReviewHandler : IRequestHandler<UpdateReviewCommand>
+	{
+		private readonly IRepository<Review> _repository;
+
+		public UpdateReviewHandler(IRepository<Review> repository)
+		{
+			_repository = repository;
+		}
+
+		public async Task Handle(UpdateReviewCommand request, CancellationToken cancellationToken)
+		{
+			var values = await _repository.GetByIdAsync(request.ReviewId);
+			values.CustomerName = request.CustomerName;
+			values.CustomerImage = request.CustomerImage;
+			values.Comment = request.Comment;
+			values.RaytingValue = request.RaytingValue;
+			values.ReviewDate = request.ReviewDate;
+			values.CarId = request.CarId;
+			await _repository.UpdateAsync(values);
+
+
+		}
+	}
+}
