65bbb0f3b5e6941d75204127ec0221b49569e846
Erdem <erdemsincer123@icloud.com>
2025-02-18 16:01:18 +0300

    Giriş ekranı

---
+++ Frontends/CarBook.webUI/Controllers/LoginController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.webUI.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
